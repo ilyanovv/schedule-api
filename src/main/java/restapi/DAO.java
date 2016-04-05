f2037808149855cb1519bfb0d90bda675a857e16
vj711 <CSKAchampion2013>
2016-04-05 17:16:48 +0300

    Deploy

--- src/main/java/restapi/DAO.java
+++ src/main/java/restapi/DAO.java
@@ -87,36 +87,22 @@ public class DAO {
         Connection c = getCon();
         JSONArray jsonArray = new JSONArray();
         String groupNumber = "8О-408Б";
-        /*PreparedStatement ps = c.prepareStatement("SELECT w.group_number, w.time_begin, w.time_end, w.lesson_type_name, w.lecture_room_number, \n" +
-                "\t\tw.building_name, t.last_name, t.first_name, t.patronymic_name FROM (\n" +
-                "\tSELECT z.group_number, z.time_begin, z.time_end, z.lesson_type_name, z.lecture_room_number, \n" +
-                "\t\tdid, z.building_name, lesson_teacher.teacher_id FROM (\t\n" +
-                "\t\tSELECT y.group_number, y.time_begin, y.time_end, y.lesson_type_name, y.lecture_room_number, \n" +
-                "\t\ty.discipline_id, y.bid, building.building_name, y.did  FROM (\n" +
-                "   \t \tSELECT group_tab.group_number, lesson_time.time_begin, lesson_time.time_end, lesson_type.lesson_type_name, lecture_room.lecture_room_number, \n" +
-                "\t\tx.discipline_id, lecture_room.building_id as bid, x.discipline_id as did\n" +
-                "\t\tFROM schedule_tab AS x\n" +
-                "\t\t\tJOIN group_tab ON x.group_id = group_tab.group_id AND group_tab.group_number = ? \n" +
-                "\t\t\tJOIN lesson_time ON lesson_time.time_id = x.time_id\n" +
-                "\t\t\tJOIN lesson_type ON lesson_type.lesson_type_id = x.lesson_type_id\n" +
-                "\t\t\tJOIN lecture_room ON lecture_room.lecture_room_id = x.lecture_room_id) AS y\n" +
-                "\tJOIN building ON bid = building.building_id) AS z \n" +
-                "JOIN lesson_teacher ON did = lesson_teacher.discipline_id) AS w\n" +
-                "JOIN teacher AS t ON w.teacher_id = t.teacher_id");*/
         PreparedStatement ps = c.prepareStatement(SQLQueries.getScheduleForGroup);
         ps.setString(1, groupNumber);
         ResultSet resultSet = ps.executeQuery();
         while (resultSet.next())
         {
             JSONObject resultJson = new JSONObject();
-            resultJson.put("time_begin", resultSet.getString("time_begin"));
-            resultJson.put("time_end", resultSet.getString("time_end"));
+            resultJson.put("time_begin", resultSet.getTime("time_begin"));
+            resultJson.put("time_end", resultSet.getTime("time_end"));
             resultJson.put("lesson_type_name", resultSet.getString("lesson_type_name"));
             resultJson.put("lecture_room_number", resultSet.getString("lecture_room_number"));
             resultJson.put("building_name", resultSet.getString("building_name"));
             resultJson.put("last_name", resultSet.getString("last_name"));
             resultJson.put("first_name", resultSet.getString("first_name"));
             resultJson.put("patronymic_name", resultSet.getString("patronymic_name"));
+            resultJson.put("lesson_date", resultSet.getDate("lesson_date"));
+            resultJson.put("lesson_name", resultSet.getString("lesson_name"));
             jsonArray.add(resultJson);
         }
         return  jsonArray;
--- src/main/java/restapi/SQLQueries.java
+++ src/main/java/restapi/SQLQueries.java
@@ -6,14 +6,15 @@ package restapi;
 public class SQLQueries {
     public static final String getScheduleForGroup = ""+
             "SELECT * FROM lesson JOIN \n" +
-            "(SELECT w.lesson_id, w.group_number, w.time_begin, w.time_end, w.lesson_type_name, w.lecture_room_number, \n" +
+            "(SELECT DATE_FORMAT(w.lesson_date, '%m.%d.%Y') AS lesson_date,\n" +
+            "        w.lesson_id, w.group_number, w.time_begin, w.time_end, w.lesson_type_name, w.lecture_room_number, \n" +
             "\t\tw.building_name, t.last_name, t.first_name, t.patronymic_name FROM (\n" +
-            "\tSELECT z.group_number, z.time_begin, z.time_end, z.lesson_type_name, z.lecture_room_number, \n" +
+            "\tSELECT z.lesson_date, z.group_number, z.time_begin, z.time_end, z.lesson_type_name, z.lecture_room_number, \n" +
             "\t\tdid, z.building_name, lesson_teacher.teacher_id, lesson_teacher.lesson_id FROM (\t\n" +
             "\t\tSELECT y.group_number, y.time_begin, y.time_end, y.lesson_type_name, y.lecture_room_number, \n" +
-            "\t\ty.discipline_id, y.bid, building.building_name, y.did  FROM (\n" +
+            "\t\ty.discipline_id, y.bid, building.building_name, y.did, y.lesson_date  FROM (\n" +
             "   \t \tSELECT group_tab.group_number, lesson_time.time_begin, lesson_time.time_end, lesson_type.lesson_type_name, lecture_room.lecture_room_number, \n" +
-            "\t\tx.discipline_id, lecture_room.building_id as bid, x.discipline_id as did\n" +
+            "\t\tx.discipline_id, lecture_room.building_id as bid, x.discipline_id as did, x.lesson_date \n" +
             "\t\tFROM schedule_tab AS x\n" +
             "\t\t\tJOIN group_tab ON x.group_id = group_tab.group_id AND group_tab.group_number = ? \n" +
             "\t\t\tJOIN lesson_time ON lesson_time.time_id = x.time_id\n" +
@@ -22,5 +23,5 @@ public class SQLQueries {
             "\tJOIN building ON bid = building.building_id) AS z \n" +
             "JOIN lesson_teacher ON did = lesson_teacher.discipline_id) AS w\n" +
             "JOIN teacher AS t ON w.teacher_id = t.teacher_id) AS a\n" +
-            "ON lesson.lesson_id = a.lesson_id\n";
+            "ON lesson.lesson_id = a.lesson_id";
 }
