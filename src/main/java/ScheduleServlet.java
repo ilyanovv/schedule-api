17e4737617c84e2f6e50441d2a01345c42eaa7f1
vj711 <CSKAchampion2013>
2016-04-05 17:55:35 +0300

    Deploy

--- src/main/java/ScheduleServlet.java
+++ src/main/java/ScheduleServlet.java
@@ -20,7 +20,7 @@ public class ScheduleServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            String groupNumber = request.getParameter("groupNumber");
+            String groupNumber = new String(request.getParameter("groupNumber").getBytes("Unicode"));
             String arr = DAO.getScheduleJSON(groupNumber).toString();
             response.setContentType("application/json");
             response.setCharacterEncoding("utf-8");
