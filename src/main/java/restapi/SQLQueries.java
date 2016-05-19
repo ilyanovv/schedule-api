a64aecf5490bf4ec8b4d5c7a09675fa50eea534d
vj711 <CSKAchampion2013>
2016-05-19 21:49:27 +0300

    Deploy

--- src/main/java/restapi/SQLQueries.java
+++ src/main/java/restapi/SQLQueries.java
@@ -15,11 +15,10 @@ class SQLQueries {
             "\t\tdid, z.building_name, lesson_teacher.teacher_id, lesson_teacher.lesson_id FROM (\t\n" +
             "\t\tSELECT y.group_number, y.time_begin, y.time_end, y.lesson_type_name, y.lecture_room_number, \n" +
             "\t\ty.discipline_id, y.record_id, y.bid, building.building_name, y.did, y.lesson_date  FROM (\n" +
-            "   \t \tSELECT group_tab.group_number, lesson_time.time_begin, lesson_time.time_end, lesson_type.lesson_type_name, lecture_room.lecture_room_number, \n" +
+            "   \t \tSELECT group_tab.group_number, time_begin, time_end, lesson_type.lesson_type_name, lecture_room.lecture_room_number, \n" +
             "\t\tx.discipline_id, lecture_room.building_id as bid, x.discipline_id as did, x.lesson_date, x.record_id \n" +
             "\t\tFROM schedule_tab AS x\n" +
             "\t\t\tJOIN group_tab ON x.group_id = group_tab.group_id AND group_tab.group_id = ? \n" +
-            "\t\t\tJOIN lesson_time ON lesson_time.time_id = x.time_id\n" +
             "\t\t\tJOIN lesson_type ON lesson_type.lesson_type_id = x.lesson_type_id\n" +
             "\t\t\tJOIN lecture_room ON lecture_room.lecture_room_id = x.lecture_room_id) AS y\n" +
             "\tJOIN building ON bid = building.building_id) AS z \n" +
