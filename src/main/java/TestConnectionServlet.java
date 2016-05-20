c67cf9fcb654e7d113de6ef3005c503e22695160
vj711 <CSKAchampion2013>
2016-05-20 03:35:06 +0300

    Deploy

---
+++ src/main/java/TestConnectionServlet.java
@@ -0,0 +1,45 @@
+import restapi.DAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by Илья on 15.05.2016.
+ */
+@WebServlet(name = "TestConnectionServlet", urlPatterns = "/test_connection")
+public class TestConnectionServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            request.setCharacterEncoding("utf-8");
+            String login = request.getParameter("login");
+            String password = request.getParameter("password");
+            Connection connection = DAO.getCon(login, password);
+            //TODO: возможно, добавить какой-нибудь запрос к БД для проверки полномочий
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT); //204
+
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            String arr = "Error has occured. Access denied";
+            response.setContentType("application/json");
+            response.setCharacterEncoding("utf-8");
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            PrintWriter out = response.getWriter();
+            out.print(arr);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
