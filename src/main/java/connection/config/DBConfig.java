37bb79c03c701ddf43364d1f683c8592911210f5
ilyanovv <ilyanovv@gmail.com>
2018-02-14 22:39:15 +0300

    OpenshiftConnection

--- src/main/java/connection/config/DBConfig.java
+++ src/main/java/connection/config/DBConfig.java
@@ -1,11 +1,19 @@
 package connection.config;
 
+import connection.DBConnection;
+import connection.LocalConnection;
+import connection.OpenshiftConnection;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {
-        "connection"
-})
+//@ComponentScan(basePackages = {
+//        "connection"
+//})
 public class DBConfig {
+    @Bean
+    DBConnection dbConnection() {
+        return new OpenshiftConnection();
+    }
 }
